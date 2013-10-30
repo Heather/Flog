29d3deca94d47cdb109230fb0019028459edac02
Heather <heather@cynede.net>
2013-10-30 16:39:19 +0400

    fix build script

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
 ::Env
 if %PROCESSOR_ARCHITECTURE%==x86 (
-	set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+	set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 ) else (
-	set MSBUILD=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
+	set MSBUILD="%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 )
 
 ::Clean
@@ -11,10 +11,10 @@ rm -rf bin
 rm -rf obj
 
 ::Build
-%MSBUILD% Flog.fsproj /p:Configuration=Release
+%MSBUILD% Flog.fsproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5.1
 
 ::Test
-%MSBUILD% example/example.fsproj /p:Configuration=Release
+%MSBUILD% example/example.fsproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5.1
 example\bin\Release\example_2012.exe
 
 ::Clean test
